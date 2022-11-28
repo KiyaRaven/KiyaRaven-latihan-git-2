e4fb1256d8f7237ef1448bd1ffa4ad0c60dd45bf
KiyaRaven <azkiyamutawally44@gmail.com>
2022-11-28 17:14:18 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h2>HALLO GESSSS</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sit nihil, pariatur, perferendis doloribus possimus ullam quis veniam mollitia quam esse consequuntur sint modi aliquid autem dolorum cum aperiam explicabo.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
