947578262f854de9c63b6487be7d93c549813ca6
Muhammad Fathurrahman <116361188+fioMei@users.noreply.github.com>
2022-11-29 09:38:50 +0700

    Add files via upload

---
+++ KiyaRaven-latihan-git-2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan git 2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>HALLO GESSSS</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sit nihil, pariatur, perferendis doloribus possimus ullam quis veniam mollitia quam esse consequuntur sint modi aliquid autem dolorum cum aperiam explicabo.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
+
+    <h1>ga boleh itu rasis</h1>
+    <h2>APAMUNGKIN NGISING BRUTAL</h2>
+</body>
+</html>
